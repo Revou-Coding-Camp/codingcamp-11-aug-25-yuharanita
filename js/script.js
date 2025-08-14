d3903b60f8d2908fa81cacd370512c487d7fc889
Tania Indiarty Rahayu <116339469+yuharanita@users.noreply.github.com>
2025-08-14 23:59:29 +0700

    Struktur utamanya udah jadi. Yang perlu dilakuin selanjutnya: tambahin gambar-gambar, bikin lebih responsif, benerin form validation. (Note: buat form other contact, coba kasih pilihan sosmednya kalo ga ribet)

--- js/script.js
+++ js/script.js
@@ -1,8 +1,26 @@
-console.log("Hello World!");
-
 welcomeSpeech();
 
 function welcomeSpeech() {
   let userName = prompt("What is your name?");
-  if (userName !="")
+  if (userName != "") {
+    document.getElementById("user-greeting").textContent =
+      ", " + userName + "!";
+  } else {
+    document.getElementById("user-greeting").textContent = "!";
+  }
+}
+
+function sendMessage() {
+  let userName = document.getElementById("user-name").value;
+  let userEmail = document.getElementById("user-email").value;
+  let userContact = document.getElementById("user-contact").value;
+  let userMessage = document.getElementById("user-message").value;
+
+  if (userName && userEmail && userMessage) {
+    alert(
+      `Thank you for your message, ${userName}! We will get back to you at ${userEmail}.`
+    );
+  } else {
+    alert("Please fill in all required fields.");
+  }
 }
