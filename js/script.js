01c0cd2e046be31e297a19fe814ce5dfee611e98
Tania Indiarty Rahayu <116339469+yuharanita@users.noreply.github.com>
2025-08-15 23:07:46 +0700

    Revisi minor sebelum final

--- js/script.js
+++ js/script.js
@@ -35,3 +35,15 @@ function sendMessage() {
     }
   }
 }
+
+// Responsive Navbar Toggle
+function Menu(e) {
+  let list = document.querySelector("ul");
+  e.name === "menu"
+    ? ((e.name = "close"),
+      list.classList.add("top-[60px]"),
+      list.classList.add("opacity-100"))
+    : ((e.name = "menu"),
+      list.classList.remove("top-[60px]"),
+      list.classList.remove("opacity-100"));
+}
